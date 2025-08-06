292243acc6fdfa8119cc28a2f014cca5a0b5fa2f
mrobo@PAVILION <utkarshojha234@gmail.com>
2025-08-06 18:50:31 +0530

    setting up terraform 2

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -58,7 +58,7 @@ resource "aws_glue_crawler" "glue_crawler_name" {
   database_name = aws_glue_catalog_database.tf_crawler_db.name
 
   s3_target {
-    path = "s3://${aws_s3_bucket.tf_ingestion_bucket.bucket}/final_master/"
+    path = "s3://${aws_s3_bucket.tf_cleaned_bucket.bucket}/final_master/"
   }
   depends_on = [
     aws_glue_job.ingestion_glue_job,
