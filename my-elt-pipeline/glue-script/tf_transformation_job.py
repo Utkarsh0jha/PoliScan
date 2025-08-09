f8bfa97ceecb13bcef65ebb38d973ae05bcd4c12
mrobo@PAVILION <utkarshojha234@gmail.com>
2025-08-09 23:48:37 +0530

    .TF

--- my-elt-pipeline/glue-script/tf_transformation_job.py
+++ my-elt-pipeline/glue-script/tf_transformation_job.py
@@ -18,7 +18,7 @@ from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import col 
-
+ 
 # Initialize Spark and Glue Context
 sc = SparkContext()
 glueContext = GlueContext(sc)
